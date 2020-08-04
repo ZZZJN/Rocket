8300338a88bae0c2f40fb328e7064ef422c2eac5
dula-9 <1852378@tongji.edu.cn>
2020-08-04 10:59:08 +0800

    User模型完善

--- models/User.php
+++ models/User.php
@@ -2,7 +2,27 @@
 
 namespace app\models;
 
-class User extends \rhosocial\base\models\models\BaseBlameableModel
+class User extends \rhosocial\user\User
 {
     public $profileClass = Profile::class;
+
+    public function getFavorites()
+    {
+        return $this->hasMany(RecipeFavorite::class,['user_guid'=>'guid']);
+    }
+    public function getLikes()
+    {
+        return $this->hasMany(RecipeLike::class, ['user_guid' => 'guid']);
+    }
+    public function getLikedRecipes()
+    {
+        return $this->hasMany(Recipe::class,['guid'=>'recipe_guid'])
+            ->via('likes');
+    }
+    public function getFavoriteRecipes()
+    {
+        return $this->hasMany(Recipe::class,['guid'=>'recipe_guid'])
+            ->via('favorites');
+    }
+
 }
