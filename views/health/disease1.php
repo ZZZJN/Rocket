d19b2fe2b619f44d301d09117b6857a8dc6bf9f3
XevenoX <68806119+XevenoX@users.noreply.github.com>
2020-08-05 20:34:45 +0800

    health视图（补充）

---
+++ views/health/disease1.php
@@ -0,0 +1,57 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = '痛风患者健康知识';
+$this->params['breadcrumbs'][] = $this->title;
+$menu='User Menu';
+
+?>
+<div class="center">
+    <div class="divs" style="margin-top: 10px;">
+        <div class="imghover">
+            <img src="/健康知识/health-disease1.jpeg" width="420" height="280">
+        </div>
+        <div class="id" style="margin-left: 10px;font-size: 45px;">
+            <strong> &nbsp;痛风</strong>
+            <div class="infos" style="font-size: 18px;">
+                <strong><br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;——「想见未来之美」</strong>
+            </div>
+        </div>
+    </div>
+
+    <div class="intro">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        虽然有些痛风患者还没有感觉到身体有什么不适，但是随着时间的增长病情也在不断地侵蚀着人们的身体。建议痛风慢性期患者及时就医诊治，否则会导致病情进步恶化，甚至引发各种严重疾病。同时，合理膳食还有助于疾病的康复。
+    </div>
+
+    <br>
+    <div class="subtitle">
+        <strong>1.&nbsp;饮食建议</strong>
+    </div>
+    <div class="content">
+        ① 痛风慢性期患者还应当多喝水，这样有利于促进体内有害物质的可溶性，以促进尿酸排泄。与此同时，患者还要做到少喝肉汤，因为肉汤中嘌呤含量太多，会加重病情。
+    </div>
+    <div class="content">
+        ② 也可以自己多喝些山栀去酸茶，从而来降低尿酸。取菊苣、栀子、葛根、百合、桑叶，每样5克，煮成茶水来喝。菊苣、栀子是有利尿降酸的功能的，再辅助其他材料，可以使效果更加的达到极致。但是煮水还是碾磨成粉效果吸收最佳。
+    </div>
+
+    <br>
+    <div class="subtitle">
+        <strong>2.&nbsp;饮食禁忌</strong>
+    </div>
+    <div class="content">
+        ① 痛风慢性期患者应注意不吸烟、不喝酒，也不要喝咖啡和浓茶等饮品，否则会使病情反复发作。
+    </div>
+    <div class="content">
+        ② 痛风慢性期患者要严格限制高嘌呤食物的摄入量，适当吃一些低嘌呤的食物， 因为高嘌呤饮食会促使体内嘌呤继续增长，加深尿酸的浓度，进而加快病情的进步恶化。
+    </div>
+    <div class="content">
+        ③ 痛风慢性期患者要严格限制热量的摄入量。如果患者摄入过多的热量，不仅使体重增加，而且还会加速尿酸合成，从而导致痛风发作。
+    </div>
+    <br><br><br><br>
+
+</div>
+
---
+++ views/health/disease2.php
@@ -0,0 +1,80 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = '心脑血管疾病患者健康知识';
+$this->params['breadcrumbs'][] = $this->title;
+$menu='User Menu';
+
+?>
+<div class="center">
+    <div class="divs" style="margin-top: 10px;">
+        <div class="imghover">
+            <img src="/健康知识/health-disease2.jpeg" width="420" height="280">
+        </div>
+        <div class="id" style="margin-left: 10px;font-size: 45px;">
+            <strong> &nbsp;心脑血管疾病</strong>
+            <div class="infos" style="font-size: 18px;">
+                <strong><br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;——「亦蔬亦果西番柿」</strong>
+            </div>
+        </div>
+    </div>
+
+    <div class="intro">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        糖尿病的病因和发病机制沿目前公认的有遗传、环境因素、自身免疫、等有关。而饮食疗法，是一项重要的其础治疗措施，应严格和长期执行。
+    </div>
+
+    <br>
+    <div class="subtitle">
+        <strong>1.&nbsp;1型糖尿病人</strong>
+    </div>
+    <div class="content">
+        在合适的总热量、食物成、规则的餐次安排等措施的基础上，配合胰岛素治疗有利于控制高血糖和防止低血糖发生。因为高血糖是由于胰岛素分泌或作用的缺陷，或者两者同时存在所致。
+    </div>
+
+    <br>
+    <div class="subtitle">
+        <strong>2.&nbsp;2型糖尿病人</strong>
+    </div>
+    <div class="content">
+        饮食治疗有利于减轻体重，改善高血糖、脂代谢紊乱和高血压，以及减少降糖药物剂量。在食物方面应注意一下几点：糖尿病患者的主食一般以米、面为主，但是，我们比较喜欢粗杂粮，如燕麦、麦片、玉米面等，因为这些食物中有较多的无机盐、维生素，又富含膳食纤维，膳食纤维具有减低血糖作用，对控制血糖有利。
+    </div>
+
+    <br>
+    <div class="subtitle">
+        <strong>2.&nbsp;饮食建议</strong>
+    </div>
+    <div class="content">
+        糖尿病患者的蛋白质来源，大豆及其豆制品为好，一方面，其所含蛋白质量多质好；另一方面，其不含胆固醇，具有降脂作用，故可代替部分动物性食品，如肉类等。
+    </div>
+    <div class="content">
+        糖尿病患者在控制热量期间，仍感饥饿时，可食用含糖少的蔬菜，用水煮后加一些佐料拌着吃。由于蔬菜所含膳食纤维多、水分多，供热能低、具有饱腹作用，是糖尿病患者必不可少的食物。
+    </div>
+
+    <br>
+    <div class="subtitle">
+        <strong>2.&nbsp;饮食建议</strong>
+    </div>
+    <div class="content">
+        禁用食物有：白糖、红塘、葡萄糖及糖制甜食，如糖果、糕点、果酱、蜜饯、冰激凌、甜饮料等。
+    </div>
+    <div class="content">
+        另外，含碳水化合物较多的土豆、山药、芋艿、藕、蒜苗、胡萝卜等少用或食用后减少相应的主食量。
+    </div>
+    <div class="content">
+        富含饱和脂肪酸的猪油、牛油、洋油、奶油、黄油等少用，最好不用。可用植物油代替部分动物油，花生、核桃、芝麻、瓜子中含脂肪也相当多，尽量不吃或少吃或减少油类摄入。 蛋黄和动物内脏如肝、脑、腰等含胆固醇相当高，应尽量少用或不用。
+    </div>
+    <div class="content">
+        水果中含葡萄糖、果糖，能使血糖升高，故在血、尿糖控制相对稳定时，空腹血糖<7.8mmol/L或餐后2小时血糖<10mmol/L时，可在两餐或临睡前食用，但也要减少相应主食。
+    </div>
+    <div class="content">
+        酒类，主要含酒精，产热高，而其他营养素含量很少，故不饮为宜。
+    </div>
+
+    <br><br><br><br>
+
+</div>
+
