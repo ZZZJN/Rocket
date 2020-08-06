a31bc79f1a78bd46f184a55507e6abf26ae88ba5
XevenoX <68806119+XevenoX@users.noreply.github.com>
2020-08-06 13:24:50 +0800

    login页面加注册按钮

--- views/site/login.php
+++ views/site/login.php
@@ -15,4 +15,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>Please fill out the following fields to login:</p>
     <?= $result = \rhosocial\user\widgets\LoginFormWidget::widget(['model' => $model]); ?>
+    <a type="button" class="btn" style="float: left;margin-left: 195px; font-size: 15px;
+    color: #ffffff;    background-color: #337ab6;"
+       href="http://rocket/user/register/index.html">
+        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+        还没有账号吗？快来注册吧！
+    </a>
 </div>
