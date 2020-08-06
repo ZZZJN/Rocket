26354c4595042f23339744ed9ad1bc8aa8dda8b9
ZZZJN <1852670@tongji.edu.cn>
2020-08-06 10:07:42 +0800

    recipe缩略图更改。

--- widgets/views/recipe缩略图.php
+++ widgets/views/recipe缩略图.php
@@ -5,18 +5,17 @@
 /* @var $recipe_name 菜谱名称 */
 
 ?>
+<a href="<?= \yii\helpers\Url::toRoute('recipe/fenzhengrou') ?>">
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-    <a href="<?= \yii\helpers\Url::toRoute('recipe/index') ?>">
-        <div class="thumbnail">
-        <img src="<?=$recipe_imgsrc?>" alt=<?= $recipe_name?> style="padding: 12px 10px 0px 10px">
+    <div class="thumbnail">
+        <img src="<?=$recipe_imgsrc?>" alt=<?= $recipe_name?>; width="252"; height="120"; style="padding: 12px 10px 0px 10px;" >
         <div class="caption">
-            <p style="font-size:18px;text-align: center;">粉蒸肉</p>
+            <p style="font-size:18px;text-align: center;"><?= $recipe_name?></p>
         </div>
         <span>
             <img src="<?=$usr_imgsrc?>" alt=<?= $usr_name?> style="width:40px;padding:0px 0px 12px 15px">
             <span style="font-size:14px;padding-left: 5px;"><?= $usr_name?></span>
         </span>
-        </div>
-    </a>
-
+    </div>
 </div>
+</a>
