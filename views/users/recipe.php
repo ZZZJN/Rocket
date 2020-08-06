bf1d13dc0a9565624d2fd369d4f265adf15c575e
XevenoX <68806119+XevenoX@users.noreply.github.com>
2020-08-06 11:46:03 +0800

    个人中心缩略图填充

--- views/users/recipe.php
+++ views/users/recipe.php
@@ -9,8 +9,14 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="center">
     <?= app\widgets\UsersHeader::widget(['title' => '我创作的菜谱','slogan' => '唯美食与爱不可辜负']) ?>
-    <?= app\widgets\UploadRecipeButton::widget() ?>
-
+    <div style="margin-right: 80px;"><?= app\widgets\UploadRecipeButton::widget() ?></div>
+    <br><br><br><br>
+    <?= app\widgets\recipe缩略图::widget(['recipe_name'=>'番茄红汤面','recipe_imgsrc'=>'/菜谱缩略图/炙烤牛排.jpg',
+        'usr_name'=>Yii::$app->user->getIdentity()->profile->nickname,'usr_imgsrc'=>'/用户头像/测试头像navbar用.JPG']) ?>
+    <?= app\widgets\recipe缩略图::widget(['recipe_name'=>'红烧猪蹄','recipe_imgsrc'=>'/菜谱缩略图/素罗宋汤.jpg',
+        'usr_name'=>Yii::$app->user->getIdentity()->profile->nickname,'usr_imgsrc'=>'/用户头像/测试头像navbar用.JPG']) ?>
+    <?= app\widgets\recipe缩略图::widget(['recipe_name'=>'快手便当','recipe_imgsrc'=>'/菜谱缩略图/肋排薯条沙拉碗.jpg',
+        'usr_name'=>Yii::$app->user->getIdentity()->profile->nickname,'usr_imgsrc'=>'/用户头像/测试头像navbar用.JPG']) ?>
 </div>
 
 
