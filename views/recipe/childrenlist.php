a9a10033b82f595626e7aff2e1b426f8620e819e
ZZZJN <1852670@tongji.edu.cn>
2020-08-06 12:30:27 +0800

    完善静怡的childrenlist 激活儿童选项。

--- views/recipe/childrenlist.php
+++ views/recipe/childrenlist.php
@@ -50,7 +50,28 @@
         </div>
     </div>
     <div class="left">
-        <?= app\widgets\recipelistnav::widget() ?>
+        <h3 style="margin-left: 20px;color: #a03a07;">热门分类</h3>
+        <ul class="nav nav-pills nav-stacked" style="background-color: #DABEC0;margin: 20px 30% 0px 20px;padding: 5px">
+            <li><a href="<?= \yii\helpers\Url::toRoute('recipe/list') ?>">家常菜</a></li>
+            <li><a href="#">快手菜</a></li>
+            <li><a href="#">下饭菜</a></li>
+            <li><a href="#">早餐</a></li>
+            <li><a href="#">肉</a></li>
+            <li><a href="#">鱼</a></li>
+            <li><a href="#">蔬菜</a></li>
+            <li><a href="#">鸡蛋</a></li>
+            <li><a href="#">汤羹</a></li>
+            <li><a href="#">烘焙</a></li>
+            <li><a href="#">主食</a></li>
+        </ul>
+        <hr>
+        <h3 style="margin-left: 20px;color: #a03a07;">人群</h3>
+        <ul class="nav nav-pills nav-stacked" style="background-color: #DABEC0;margin:20px 30% 0px 20px;padding: 5px;">
+            <li class="active"><a href="<?= \yii\helpers\Url::toRoute('recipe/childrenlist') ?>">儿童</a></li>
+            <li><a href="#">婴幼儿</a></li>
+            <li><a href="#">老人</a></li>
+            <li><a href="#">孕产妇</a></li>
+        </ul>
     </div>
 </div>
 </body>
